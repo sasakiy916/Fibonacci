a8016973fa832f045b60030c132e94858f75733d
sasakiy916 <sasaki3y98521j@gmail.com>
2022-03-27 19:48:34 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main{
+	public static void main(String[] args){
+		int preNum = 0;;
+		int nextNum = 1;
+		int sum;
+		for(int i=1;i<=30;i++){
+			System.out.print(nextNum + ",");
+			sum = preNum + nextNum;
+			preNum = nextNum;
+			nextNum = sum;
+			if(i%5==0){
+				System.out.println();
+			}
+		}
+	}
+}
